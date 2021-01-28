31600ef1d1e40be6582c95ad4a4fd7fb6a02cafc
AkarshPanicker123 <76616282+AkarshPanicker123@users.noreply.github.com>
2021-01-28 17:14:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+var canvas;
+var ground1,ground2,ground3,ground4;
+var ball, edges;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    ground1 = createSprite(0,580,360,30);
+    ground1.shapeColor = rgb(0,0,255);
+
+    ground2 = createSprite(295,580,200,30);
+    ground2.shapeColor = rgb(255,128,0);
+
+    ground3 = createSprite(515,580,200,30);
+    ground3.shapeColor = rgb(153,0,76);
+
+    ground4 = createSprite(740,580,220,30);
+    ground4.shapeColor = rgb(0,100,0);
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+    ball.velocityX = 4;
+    ball.velocityY = 9;
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+
+    if(ground1.isTouching(ball) && ball.bounceOff(ground1)){
+        ball.shapeColor = rgb(0,0,255);
+        music.play();
+    }
+
+    if(ground2.isTouching(ball)){
+        ball.shapeColor = rgb(255,128,0);
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+        music.stop();
+    }
+
+    if(ground3.isTouching(ball) && ball.bounceOff(ground3)){
+        ball.shapeColor = rgb(153,0,76);
+    }
+
+    if(ground4.isTouching(ball) && ball.bounceOff(ground4)){
+        ball.shapeColor = rgb(0,100,0);
+    }
+
+    drawSprites();
+}
